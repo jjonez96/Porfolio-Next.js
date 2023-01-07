6bc33d673354397533c368ec5f48d7fd43223ce5
Joona Luukkonen <joona114@hotmail.fi>
2023-01-07 23:46:27 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,13 @@
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-import MyBackground from "./components/MyBackground";
-import TechStack from "./components/TechStack";
-import WorkingMethods from "./components/WorkingMethods";
-import Portfolio from "./components/Portfolio";
-import ProjectCards from "./components/ProjectCards";
-import BacktoTop from "./components/BacktoTop";
-import Footer from "./components/Footer";
+import Head from "next/head";
+import Map from "./components/Map";
 
 export default function Home() {
   return (
     <main id="top">
-      <NavBar />
-      <Header />
-      <MyBackground />
-      <TechStack />
-      <WorkingMethods />
-      <Portfolio />
-      <ProjectCards />
-      <BacktoTop />
-      <Footer />
+      <Head>
+        <title>Scootlator</title>
+      </Head>
+      <Map />
     </main>
   );
 }
