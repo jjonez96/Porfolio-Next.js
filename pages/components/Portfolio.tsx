4888d2c425b9802fb3684cc57fc1a822e4cc9727
Joona Luukkonen <joona114@hotmail.fi>
2023-01-24 20:56:55 +0200

    text changes portfolio

--- pages/components/Portfolio.tsx
+++ pages/components/Portfolio.tsx
@@ -6,9 +6,9 @@ const Portfolio = () => {
         <p className="text-md py-5 leading-6 text-neutral-400 text-left">
           Osaamiseni painottuu enemmän fronttiin ja toteutankin projektit
           pääsääntöisesti <b>Reactilla</b>. Reactin parissa on tullut
-          väkerreltyä noin vuoden verran aktiivisesti. Myös Reactiin kuuluvia
-          frameworkkejä on tullut testailtua mm. <b>Next.js ja Gatsby.js</b>.
-          Esim. Tämä portfolio sivu on tehty <b>Next.js:llä ja Tailwind CSS</b>{" "}
+          väkerreltyä yli vuoden verran aktiivisesti. Myös Reactiin kuuluvia
+          frameworkkejä on tullut testailtua mm. <b>Next.js</b>. Esim. Tämä
+          portfolio sivu on tehty <b>Next.js:llä ja Tailwind CSS</b>{" "}
           frameworkilla. Tässä{" "}
           <a
             href="https://github.com/jjonez96/porfolio"
@@ -17,11 +17,11 @@ const Portfolio = () => {
             lähdekoodi
           </a>{" "}
           portfolioon. <br /> <br /> <b>Node.js ja Express</b> on tullut
-          lähiaikoina myös kokajan tutummaksi nykyistä Scootlator projektia
-          värkkäillessä. Enimmäkseen näillä kielillä teen rajapintoja fronttiin
-          ja karsin turhaa dataa pois. <br /> <br /> Meillä alkaa koulussa
-          kevätlukukautena 2023 <b>C#, .NET ja SQL</b> kurssit, joita odotan
-          innolla kun en ole näillä päässyt vielä työskentelemään.
+          lähiaikoina myös kokoajan tutummaksi nykyistä Scootlator projektia
+          värkkäillessä. Enimmäkseen näillä teen rajapintoja fronttiin ja karsin
+          samalla turhan datan pois. <br /> <br /> Meillä alkoi koulussa
+          kevätlukukautena <b>C# ja SQL</b> kurssit, jotka ovat olleet
+          mielenkiitoisia, kun en ole näillä vielä päässyt työskentelemään.
         </p>
         <h3 className="text-lg font-medium relative top-6 text-neutral-100">
           Github repojen readme osioissa on lisätietoa projekteista.
